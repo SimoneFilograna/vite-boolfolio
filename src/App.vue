aae79541acd7d226c39581ea207c7b970c099dd0
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-10-16 16:09:03 +0200

    add-props

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <script>
+  import TheHeader from "./components/TheHeader.vue";
+  import TheFooter from "./components/TheFooter.vue";
+  import Card from "./components/Card.vue";
   import axios from "axios";
 
   export default {
+    components:{
+      TheHeader,
+      TheFooter,
+      Card,
+    },
     data() {
       return{
         projects: [],
@@ -19,8 +27,9 @@
             delete result.data;
             this.pagination = result;
           })       
-        }
+        },
       },
+      
       mounted() {
         this.getProjects()
       }
@@ -28,9 +37,23 @@
 </script>
 
 <template>
-  <div class="container">
-    <h1>hello vue</h1>
-  </div>
+  <TheHeader></TheHeader>
+  <div class="content p-5">
+    <div class="container-fluid text-light text-center p-5">
+        <h2 class="display-5 fst-italic">My works</h2>
+
+        <div class="row row-cols-3 mt-5 g-5 p-2" >
+                <div class="col mt-5" v-for="project in projects">
+                  <Card :singleCard="project"></Card>                   
+                </div> 
+        </div>
+    </div>
+
+    <!-- <div class="watch-more mt-5 text-center">
+        <a href="{{route("admin.projects.index")}}" class="btn btn-primary more-button">WATCH MORE</a>
+    </div> -->
+</div>
+<TheFooter></TheFooter>
 </template>
 
 <style>
---
+++ src/components/Card.vue
@@ -0,0 +1,27 @@
+<script>
+    export default{
+        props:{
+            singleCard: Object
+        },
+        methods:{
+            
+            // FUNZIONE PER OTTENERE L'IMMAGINE DAL PROGETTO BACKEND
+        getImg(singleCard){
+            return `http://127.0.0.1:8000/storage/${singleCard.thumb}`
+            }
+        }
+    }
+</script>
+
+<template>
+        <div class="card proj-card">
+            <img :src="getImg(singleCard)" class="card-img-top" alt="">
+            <div class="card-body d-flex flex-column">
+                <h5 class="card-title">{{singleCard.title}}</h5>
+            </div>
+        </div>
+</template>
+
+<style scoped>
+
+</style>
