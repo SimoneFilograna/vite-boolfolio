62807d6c549d28a51f4b0e0b5875826a9dd704d8
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-10-16 14:55:07 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script>
+  import axios from "axios";
+
+  export default {
+    data() {
+      return{
+        projects: []
+      }
+    },
+    methods: {
+      getProjects(){
+        axios.get("http://127.0.0.1:8000/api/projects")
+        .then((response)=>{
+            this.projects = response.data.data
+          })
+          
+        }
+      },
+      mounted() {
+        this.getProjects()
+      }
+    }
+</script>
+
+<template>
+  <div class="container">
+    <h1>hello vue</h1>
+  </div>
+</template>
+
+<style>
+
+</style>
